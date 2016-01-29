a6960acbb51057ed7041e12d1d66385086317af7
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-01-29 09:58:08 +0100

    apps: ipsec: reduce timeout
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- apps/ipsec/odp_generator.c
+++ apps/ipsec/odp_generator.c
@@ -679,7 +679,7 @@ int main(int argc, char * argv[])
 		EXAMPLE_ERR("Error: ODP local init failed.\n");
 		exit(EXIT_FAILURE);
 	}
-	my_sleep(40 + __k1_get_cluster_id() / 4);
+	my_sleep(20 + __k1_get_cluster_id() / 4);
 
 	/* init counters */
 	odp_atomic_init_u64(&counters.seq, 0);
