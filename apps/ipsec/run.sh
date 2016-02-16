5fbac288520d3b50b1ea50ab5d7bd3a7c9e5471a
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-02-16 17:27:23 +0100

    apps: change firmware name to avoid board matching
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- apps/ipsec/run.sh
+++ apps/ipsec/run.sh
@@ -1,21 +1,6 @@
 CUR_DIR=$(readlink -e $(dirname $0))
 cd $CUR_DIR
 
-BTYPE="konic80"
-if [ -e /mppa/board0/type ]; then
-	BTYPE=$(cat /mppa/board0/type)
-fi
-case "$BTYPE" in
-	"konic80")
-		FIRMWARE=firmware-konic80.kelf
-		;;
-	"ab01")
-		FIRMWARE=firmware-ab01.kelf
-		;;
-	*)
-		echo "Unsupported board"
-		exit 1
-		;;
-esac
+FIRMWARE=firmware.kelf
 
 exec k1-jtag-runner --progress --multibinary=ipsec.mpk --exec-multibin=IODDR0:${FIRMWARE} --exec-multibin=IODDR1:${FIRMWARE} --
