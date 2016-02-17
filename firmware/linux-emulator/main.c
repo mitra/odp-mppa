ba2101d44077a111aef0fb60e5c3512ea225d2cf
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-02-17 08:29:38 +0100

    firmware: linux-emulator: add emulation code for linux running in IO
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

---
+++ firmware/linux-emulator/main.c
@@ -0,0 +1,141 @@
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <mppa_noc.h>
+#include "odp_rpc_internal.h"
+
+typedef struct {
+	uint8_t dma_if;          /**< DMA Rx Interface */
+	uint8_t min_port;
+	uint8_t max_port;
+} rx_config_t;
+
+int rx_open(rx_config_t *rx_config, int n_ports)
+{
+	const int dma_if = 0;
+
+	/*
+	 * Allocate contiguous RX ports
+	 */
+	int n_rx = 0, first_rx;
+
+	for (first_rx = 0; first_rx <  MPPA_DNOC_RX_QUEUES_NUMBER - n_ports;
+	     ++first_rx) {
+		for (n_rx = 0; n_rx < n_ports; ++n_rx) {
+			mppa_noc_ret_t ret;
+			ret = mppa_noc_dnoc_rx_alloc(dma_if,
+						     first_rx + n_rx);
+			if (ret != MPPA_NOC_RET_SUCCESS)
+				break;
+		}
+		if (n_rx < n_ports) {
+			n_rx--;
+			for ( ; n_rx >= 0; --n_rx) {
+				mppa_noc_dnoc_rx_free(dma_if,
+						      first_rx + n_rx);
+			}
+		} else {
+			break;
+		}
+	}
+	if (n_rx < n_ports) {
+		assert(n_rx == 0);
+		fprintf(stderr, "failed to allocate %d contiguous Rx ports\n", n_ports);
+		return -1;
+	}
+
+	rx_config->dma_if = dma_if;
+	rx_config->min_port = first_rx;
+	rx_config->max_port = first_rx + n_rx - 1;
+	return 0;
+}
+
+int main(){
+	int ret;
+	unsigned cluster_id = __k1_get_cluster_id();
+	odp_rpc_t *ack_msg;
+	rx_config_t rx_config;
+	odp_rpc_cmd_ack_t ack;
+
+	for (volatile int i = 0; i < 20000000; ++i);
+	odp_rpc_client_init();
+	{
+		odp_rpc_cmd_eth_dual_mac_t mac_cmd = {
+			.enabled = 1
+		};
+		odp_rpc_t cmd = {
+			.data_len = 0,
+			.pkt_type = ODP_RPC_CMD_ETH_DUAL_MAC,
+			.inl_data = mac_cmd.inl_data,
+			.flags = 0,
+		};
+
+		odp_rpc_do_query(odp_rpc_get_io_dma_id(1, cluster_id),
+					 odp_rpc_get_io_tag_id(cluster_id),
+					 &cmd, NULL);
+		ret = odp_rpc_wait_ack(&ack_msg, NULL, 15 * RPC_TIMEOUT_1S);
+		if (ret < 0) {
+			fprintf(stderr, "[ETH] RPC Error\n");
+			return 1;
+		} else if (ret == 0){
+			fprintf(stderr, "[ETH] Query timed out\n");
+			return 1;
+		}
+		ack.inl_data = ack_msg->inl_data;
+		if (ack.status) {
+			fprintf(stderr, "[ETH] Error: Server declined dual mac mode\n");
+			return 1;
+		}
+	}
+
+	ret = rx_open(&rx_config, 20);
+	if (ret) {
+		fprintf(stderr, "Failed to open Rx ports\n");
+	}
+
+	{
+		/*
+		 * RPC Msg to IOETH  #N so the LB will dispatch to us
+		 */
+		odp_rpc_cmd_eth_open_t open_cmd = {
+			{
+				.ifId = 4,
+				.dma_if = rx_config.dma_if,
+				.min_rx = rx_config.min_port,
+				.max_rx = rx_config.max_port,
+				.loopback = 0,
+				.jumbo = 0,
+				.rx_enabled = 1,
+				.tx_enabled = 1,
+				.nb_rules = 0,
+			}
+		};
+		odp_rpc_t cmd = {
+			.data_len = 0,
+			.pkt_type = ODP_RPC_CMD_ETH_OPEN_DEF,
+			.inl_data = open_cmd.inl_data,
+			.flags = 0,
+		};
+
+		odp_rpc_do_query(odp_rpc_get_io_dma_id(1, cluster_id),
+				 odp_rpc_get_io_tag_id(cluster_id),
+				 &cmd, NULL);
+
+		ret = odp_rpc_wait_ack(&ack_msg, NULL, -1);
+		if (ret < 0) {
+			fprintf(stderr, "[ETH] RPC Error\n");
+			return 1;
+		} else if (ret == 0){
+			fprintf(stderr, "[ETH] Query timed out\n");
+			return 1;
+		}
+		ack.inl_data = ack_msg->inl_data;
+		if (ack.status) {
+			fprintf(stderr, "[ETH] Error: Server declined opening of eth interface\n");
+			return 1;
+		}
+	}
+	printf("Ethernet opened successfully\n");
+	while(1) {}
+	return 0;
+}
