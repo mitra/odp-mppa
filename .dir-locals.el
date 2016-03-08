7709f5819a0c22f936ac7c49edfabf056d1abaf0
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2016-03-08 11:05:30 +0100

    .dir-locals.el: fix indent settings
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,5 @@
-((nil . ( (c-basic-offset . 8)
-			  (tab-width . 8)
-			  (indent-tabs-mode . t)
-			  (show-trailing-whitespace . t)
-			  (c-offsets-alist
-			   (arglist-cont-nonempty
-				c-lineup-gcc-asm-reg
-				c-lineup-arglist-tabs-only))
-			  )))
+((nil . ((c-basic-offset . 8)
+	 (tab-width . 8)
+	 (indent-tabs-mode . t)
+	 (show-trailing-whitespace . t)
+	)))
